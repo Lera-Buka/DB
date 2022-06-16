6ae1b8ea7f0c340fac89aa4885e9ee3a6ff73299
Lera-Buka <79201188+Lera-Buka@users.noreply.github.com>
2022-06-16 15:01:42 +0300

    Add files via upload

---
+++ lab8/topics (3).sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 16 2022 г., 14:58
+-- Версия сервера: 8.0.24
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `topics`
+--
+
+CREATE TABLE `topics` (
+  `id` int NOT NULL,
+  `topic_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `topics`
+--
+
+INSERT INTO `topics` (`id`, `topic_name`) VALUES
+(1, 'Операційні системи'),
+(2, 'Використання ПК в цілому'),
+(3, 'Прорамування');
+
+--
+-- Триггеры `topics`
+--
+DELIMITER $$
+CREATE TRIGGER `trig1` BEFORE INSERT ON `topics` FOR EACH ROW IF (SELECT COUNT(*) FROM topics)> 10
+ THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Кількість тем більше 10';
+ ELSEIF (SELECT COUNT(*) FROM topics)< 5
+ THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Кількість тем менша за 5';
+ END IF
+$$
+DELIMITER ;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `topics`
+--
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
